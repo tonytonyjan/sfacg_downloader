63388f7d8a689f143cfe6a5c036bd6a127ecf4b2
Tony Jian <tonytonyjan@gmail.com>
2014-08-24 16:11:55 +0800

    update desc

--- lib/sfacg/cli.rb
+++ lib/sfacg/cli.rb
@@ -4,14 +4,22 @@ require 'thor'
 
 module Sfacg
   class Cli < Thor
-    desc 'comic URL', 'Download a comic, ex: http://comic.sfacg.com/HTML/JJDJR/'
-    option :to, default: '.'
+    desc 'comic URL', 'Download a comic'
+    long_desc <<-EOS
+      Download a comic:
+      \x5 $ sfacg chapter http://comic.sfacg.com/HTML/JJDJR/ --to /tmp
+    EOS
+    option :to, default: '.', banner: '<dir>', desc: 'Where to save.'
     def comic url
       Comic.new(url).download to: options[:to]
     end
 
-    desc 'chapter URL', 'Download a chapter, ex: http://comic.sfacg.com/HTML/JJDJR/056/'
-    option :to, default: '.'
+    desc 'chapter URL', 'Download a chapter'
+    long_desc <<-EOS
+      Download a chapter:
+      \x5 $ sfacg chapter http://comic.sfacg.com/HTML/JJDJR/056/ --to /tmp
+    EOS
+    option :to, default: '.', banner: '<dir>', desc: 'Where to save.'
     def chapter url
       Chapter.new(url).download to: options[:to]
     end
