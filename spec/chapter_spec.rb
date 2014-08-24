d58311cf7b929a863bae7d78a4f246864b6e40ed
Tony Jian <tonytonyjan@gmail.com>
2014-08-24 14:37:52 +0800

    consistancy

--- spec/chapter_spec.rb
+++ spec/chapter_spec.rb
@@ -14,21 +14,21 @@ describe Sfacg::Chapter do
   it '#images' do
     images = @chapter.images
     expect(images.map(&:to_s)).to match_array [
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/001_17670.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/002_22736.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/003_9374.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/004_14113.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/005_15235.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/006_24967.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/007_24880.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/008_11142.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/009_21826.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/010_20426.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/011_812.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/012_8585.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/013_30872.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/014_2534.png",
-      "http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/015_19383.png"
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/001_17670.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/002_22736.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/003_9374.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/004_14113.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/005_15235.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/006_24967.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/007_24880.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/008_11142.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/009_21826.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/010_20426.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/011_812.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/012_8585.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/013_30872.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/014_2534.png',
+      'http://hotpic.sfacg.com/Pic/OnlineComic1/OnePiece/757/015_19383.png'
     ]
   end
 end
